841b2f8e97d3fed4b9191778e2a77f3e8244371e
Alwin Ray <alwinray37@gmail.com>
2023-06-06 10:30:32 -1000

    added the trip section, camping gear section, and travel essentials section

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'test title',
+        createdAt: new Date(),
+        description: 'Test Description'
+    }]
+    res.render('index', { articles: articles })
+})
+
+app.listen(5001)
